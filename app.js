7c052261daa8000ed48431e4205c5a2055d88fd5
Deep Bhattacharjee <deepbhattacharjee486@gmail.com>
2019-03-21 12:49:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var request = require('request');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res){
+    res.render('landing');
+});
+
+app.get('/results', function(req,res){
+    var query = req.query.search;
+    var url = 'http://www.omdbapi.com/?s='+query+'&apikey=852159f0';
+    request(url, function(error,response,body){
+        if(!error && response.statusCode==200){
+            var data = JSON.parse(body);
+            res.render('results',{data:data});
+        }
+    });
+});
+
+app.listen("3000", function(){
+    console.log("Server Has Started");
+});
